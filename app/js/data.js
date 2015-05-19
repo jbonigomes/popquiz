9fd345441a318a8b1a31bc27f387875bb9449786
Jose B Gomes <jbonigomes@yahoo.com.br>
2015-05-19 19:47:25 +0100

    Adding I dont know answers

--- app/js/data.js
+++ app/js/data.js
@@ -3,70 +3,70 @@ var questions = [
 {
   "question": "Discuss how the following object-oriented concepts help a programmer design and implement an application. Illustrate your answer with appropriate examples: encapsulation; inheritance, polymorphism",
   "color": "orange",
-  "answer": ""
+  "answer": "I don't know"
 },
 {
   "question": "Define the term uniform access principle.",
   "color": "orange",
-  "answer": ""
+  "answer": "I don't know"
 },
 
 // Evaluation
 {
   "question": "Distinguish between lazy and eager evaluation in the implementation of applicative (functional) programming languages, illustrating your answer with suitable examples.",
   "color": "pastel",
-  "answer": ""
+  "answer": "I don't know"
 },
 
 // Types
 {
   "question": "Briefly describe the main differences between weakly typed and strongly typed languages. What are the relative advantages and disadvantages?",
   "color": "purple",
-  "answer": ""
+  "answer": "I don't know"
 },
 
 // Functional
 {
   "question": "Explain what it means for a function call to be tail-recursive",
   "color": "blue",
-  "answer": ""
+  "answer": "I don't know"
 },
 {
   "question": "Briefly explain the concept of referential transparency. You should provide appropriate examples using functional and imperative programming languages",
   "color": "blue",
-  "answer": ""
+  "answer": "I don't know"
 },
 
 // Prolog
 {
   "question": "Briefly explain the term non-monotonic logic.",
   "color": "red",
-  "answer": ""
+  "answer": "I don't know"
 },
 {
   "question": "What is a rule and what is a fact?"
   "color": "red",
-  "answer": ""
+  "answer": "I don't know"
 },
 {
   "question": "What is the difference between a procedure a method and a function?",
   "color": "red",
-  "answer": ""
+  "answer": "I don't know"
 },
 
 // General
 {
   "question": "State briefly what the Blub Paradox is.",
   "color": "dark",
-  "answer": ""
+  "answer": "I don't know"
 },
 {
   "question": "Define the term atomic operation.",
   "color": "dark",
-  "answer": ""
+  "answer": "I don't know"
 },
 {
   "question": "List the advantages and disadvantages of the paradigms."
   "color": "dark",
-  "answer": ""
+  "answer": "I don't know"
 }];
