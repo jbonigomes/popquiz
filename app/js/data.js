5b785879b6c6111460d2066bbb01a09533a1d6a3
Jose B Gomes <jbonigomes@yahoo.com.br>
2015-05-19 19:46:50 +0100

    Adding the skeleton for questions

--- app/js/data.js
+++ app/js/data.js
@@ -1,60 +1,72 @@
-var questions = [{
-  question: 'Briefly describe the main differences between weakly typed and strongly typed languages',
-  color: 'yellow',
-  answer: ''
+var questions = [
+// OOP
+{
+  "question": "Discuss how the following object-oriented concepts help a programmer design and implement an application. Illustrate your answer with appropriate examples: encapsulation; inheritance, polymorphism",
+  "color": "orange",
+  "answer": ""
 },
 {
-  question: 'Explain what it means for a function call to be tail-recursive',
-  color: 'blue',
-  answer: ''
+  "question": "Define the term uniform access principle.",
+  "color": "orange",
+  "answer": ""
+},
+
+// Evaluation
+{
+  "question": "Distinguish between lazy and eager evaluation in the implementation of applicative (functional) programming languages, illustrating your answer with suitable examples.",
+  "color": "pastel",
+  "answer": ""
+},
+
+// Types
+{
+  "question": "Briefly describe the main differences between weakly typed and strongly typed languages. What are the relative advantages and disadvantages?",
+  "color": "purple",
+  "answer": ""
+},
+
+// Functional
+{
+  "question": "Explain what it means for a function call to be tail-recursive",
+  "color": "blue",
+  "answer": ""
 },
 {
-  question: 'Define the term uniform access principle.',
-  color: 'blue',
-  answer: ''
+  "question": "Briefly explain the concept of referential transparency. You should provide appropriate examples using functional and imperative programming languages",
+  "color": "blue",
+  "answer": ""
+},
+
+// Prolog
+{
+  "question": "Briefly explain the term non-monotonic logic.",
+  "color": "red",
+  "answer": ""
 },
 {
-  question: 'Briefly explain the concept of referential transparency. You should provide appropriate examples using functional and imperative programming languages',
-  color: 'blue',
-  answer: ''
+  "question": "What is a rule and what is a fact?"
+  "color": "red",
+  "answer": ""
 },
 {
-  question: 'Briefly describe the main differences between weakly typed and strongly typed languages.',
-  color: 'blue',
-  answer: ''
+  "question": "What is the difference between a procedure a method and a function?",
+  "color": "red",
+  "answer": ""
+},
+
+// General
+{
+  "question": "State briefly what the Blub Paradox is.",
+  "color": "dark",
+  "answer": ""
 },
 {
-  question: 'Briefly explain the concept of referential transparency. You should provide appropriate examples using functional and imperative programming languages.',
-  color: 'blue',
-  answer: ''
+  "question": "Define the term atomic operation.",
+  "color": "dark",
+  "answer": ""
 },
 {
-  question: 'Discuss how the following object-oriented concepts help a programmer design and implement an application. Illustrate your answer with appropriate examples: encapsulation; inheritance, polymorphism',
-  color: 'blue',
-  answer: ''
-},
-{
-  question: 'Distinguish between lazy and eager evaluation in the implementation of applicative (functional) programming languages, illustrating your answer with suitable examples.',
-  color: 'blue',
-  answer: ''
-},
-{
-  question: '',
-  color: 'blue',
-  answer: ''
-},
-{
-  question: '',
-  color: 'blue',
-  answer: ''
-},
-{
-  question: '',
-  color: 'blue',
-  answer: ''
-},
-{
-  question: '',
-  color: 'blue',
-  answer: ''
+  "question": "List the advantages and disadvantages of the paradigms."
+  "color": "dark",
+  "answer": ""
 }];
